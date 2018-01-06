24b83ea89faa75b0e52ddb05be9b3779d96e4b1b
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-06 13:11:34 +0100

    msm8998: pocketmode: auto-detect proximity state file
    Change-Id: I2f11a28ed06eb6aceae04c8125e411defe7f882a

--- pocketmode/src/com/cyanogenmod/pocketmode/ProximitySensor.java
+++ pocketmode/src/com/cyanogenmod/pocketmode/ProximitySensor.java
@@ -48,16 +48,30 @@ public class ProximitySensor implements SensorEventListener {
                 mContext.getSystemService(Context.SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
-        if (android.os.Build.DEVICE.equals("OnePlus5")) {
+        if (fileExists(CHEESEBURGER_FILE)) {
             FPC_FILE = CHEESEBURGER_FILE;
-        } else {
+            found = true;
+        } else if (fileExists(DUMPLING_FILE)) {
             FPC_FILE = DUMPLING_FILE;
+            found = true;
+        } else {
+            Log.e(TAG, "No proximity state file found!");
+            FPC_FILE = CHEESEBURGER_FILE;
+        }
+
+        if (found) {
+            if (DEBUG) Log.d(TAG, "Using proximity state from " + FPC_FILE);
         }
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        setFPProximityState(event.values[0] < mSensor.getMaximumRange());
+        boolean isNear = event.values[0] < mSensor.getMaximumRange();
+        if (isFileWritable(FPC_FILE)) {
+            writeLine(FPC_FILE, isNear ? "1" : "0");
+        } else {
+            Log.e(TAG, "Proximity state file " + FPC_FILE + " is not writable!");
+        }
     }
 
     @Override
@@ -82,7 +96,56 @@ public class ProximitySensor implements SensorEventListener {
     protected void disable() {
         if (DEBUG) Log.d(TAG, "Disabling");
         mSensorManager.unregisterListener(this, mSensor);
-        // Ensure FP is left enabled
-        setFPProximityState(/* isNear */ false);
+    }
+
+    /**
+     * Checks whether the given file exists
+     *
+     * @return true if exists, false if not
+     */
+    public static boolean fileExists(String fileName) {
+        final File file = new File(fileName);
+        return file.exists();
+    }
+
+    /**
+     * Checks whether the given file is writable
+     *
+     * @return true if writable, false if not
+     */
+    public static boolean isFileWritable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canWrite();
+    }
+
+    /**
+     * Writes the given value into the given file
+     *
+     * @return true on success, false on failure
+     */
+    public static boolean writeLine(String fileName, String value) {
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(value);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for writing", e);
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Could not write to file " + fileName, e);
+            return false;
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return true;
+>>>>>>> 31ca935... msm8998: pocketmode: auto-detect proximity state file
     }
 }
